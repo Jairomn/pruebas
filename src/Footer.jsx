5622d3fbb68d43f59f00ee106177e085d153f135
Jairo <jairomanueln@gmail.com>
2024-03-06 18:53:31 -0300

    Agregue funcionalidad en btn social media

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,37 +5,31 @@ const Footer = () => {
         <div className="container p-4 pb-0">
           {/* Section: Social media */}
           <section className="mb-4">
+            
             {/* Facebook */}
             <a
               data-mdb-ripple-init
               className="btn text-white btn-floating m-1"
               style={{ backgroundColor: "#3b5998" }}
-              href="#!"
+              href="https://www.facebook.com/LosrodriguezTucuman"
+              target="_blank"
+              rel="noopener noreferrer"
               role="button"
             >
               <i className="bi bi-facebook"></i>
             </a>
   
-            {/* Twitter */}
+            {/* WhatsApp */}
             <a
               data-mdb-ripple-init
               className="btn text-white btn-floating m-1"
-              style={{ backgroundColor: "#55acee" }}
-              href="#!"
+              style={{ backgroundColor: "#25d366" }}
+              href="https://l.facebook.com/l.php?u=https%3A%2F%2Fapi.whatsapp.com%2Fsend%3Fphone%3D5493813379903%26text%3DEnlace%253A%250Ahttps%253A%252F%252Ffb.me%252F3geIXPN0Z%250A%250A%25C2%25A1Hola!%2520Podr%25C3%25ADas%2520darme%2520m%25C3%25A1s%2520informaci%25C3%25B3n%2520de...%26source_url%3Dhttps%253A%252F%252Ffb.me%252F3geIXPN0Z%26icebreaker%3D%25C2%25A1Hola!%2520Podr%25C3%25ADas%2520darme%2520m%25C3%25A1s%2520informaci%25C3%25B3n%2520de...%26app%3Dfacebook%26entry_point%3Dpost_cta%26jid%3D5493813379903%2540s.whatsapp.net%26show_keyboard%3D1%26show_ad_attribution%3D1%26context%3DARBKRFZa8GKQiqhwzGCLBRLkxWMsw3b1i-Ijke7n1444hEohTvP_GLXIGB9z2Cbx5X9p-kO0mEqkVtwrAraMcMRNpZG6VhGl-2Vxlo5TBaI_GC7eyug3zAaLx3l1uA1X00aWYzOJ_Y_Vrn28D47ENlKsInExN9qGg-PG3oRMC2scroK0B-ckQXNQ8EUvrjSp-eeI43IsJ_enMrd0y0VoeYBpS6bjxXP-uVQrPDOmHu3OezK9dYq0UjulGzwPMplBWPkQvz2ucjRGkvYEukNUG9vugRz_W3HE4FUxWFi6cONymRwFfl2C7HNTw0lNiTR5ou6eI4sXbL3Jo18eVa_YcQ%26token%3DeyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjEyNSJ9.eyJleHAiOjE3MDk4NDczNDEsInBob25lIjoiNTQ5MzgxMzM3OTkwMyIsInRleHQiOiJFbmxhY2U6XG5odHRwczpcL1wvZmIubWVcLzNnZUlYUE4wWlxuXG5cdTAwYTFIb2xhISBQb2RyXHUwMGVkYXMgZGFybWUgbVx1MDBlMXMgaW5mb3JtYWNpXHUwMGYzbiBkZS4uLiIsInNvdXJjZV91cmwiOiJodHRwczpcL1wvZmIubWVcLzNnZUlYUE4wWiIsImljZWJyZWFrZXIiOiJcdTAwYTFIb2xhISBQb2RyXHUwMGVkYXMgZGFybWUgbVx1MDBlMXMgaW5mb3JtYWNpXHUwMGYzbiBkZS4uLiIsImFwcCI6ImZhY2Vib29rIiwiZW50cnlfcG9pbnQiOiJwb3N0X2N0YSIsImppZCI6IjU0OTM4MTMzNzk5MDNcdTAwNDBzLndoYXRzYXBwLm5ldCIsInNob3dfa2V5Ym9hcmQiOjEsInNob3dfYWRfYXR0cmlidXRpb24iOjF9.bC9oVveJXyK1VtieL5AgiT9UE7IX9VY83XeJEZQS4_lVejga0eopB441FXfnR6kYTq1W5DHdBjVaKHO2G5ejVA%26fbclid%3DIwAR0l2k3J8707xy0BRGwb4vu7h8pSz6ORLrGS4suJQN9OJg6w8vCYl13HFf8&h=AT1GX-4-lr4OfwyeGygEUEPZUr66Nfluhf_BaTSR4i35uxO3V61onhY5o1zLwbCCiKEb8GQ5u-PGOUkVEqyC7DEjkJ-wYKZQp90k_dy1jYPFpsMA9nSgomjvKshOAuf-EgbE3UpUiGWg_vgR2CI&__tn__=*J%2CmH-R&c[0]=AT3dB67Olj7gwClV1V0lHDIKjYHrGoSolyMbIu8_MAhDkGQdHSkpL0IAAKV5thLxJFYOzrm-0qD9xWJzJ---DBsxrWV1Oc9PO8H7MS5769QbiLHjuE8bJ4o1AEKZylBmQe4k8Pgs_yDcz89-JK8Z543v2qKWZppETsahM_JmAUgflMW35l5mUTP5SAJJCu_Pf5ioxeh_TN3p"
+              target="_blank"
+              rel="noopener noreferrer"
               role="button"
             >
-              <i className="bi bi-twitter"></i>
-            </a>
-  
-            {/* Google */}
-            <a
-              data-mdb-ripple-init
-              className="btn text-white btn-floating m-1"
-              style={{ backgroundColor: "#dd4b39" }}
-              href="#!"
-              role="button"
-            >
-              <i className="bi bi-google"></i>
+              <i className="bi bi-whatsapp"></i>
             </a>
   
             {/* Instagram */}
@@ -43,23 +37,15 @@ const Footer = () => {
               data-mdb-ripple-init
               className="btn text-white btn-floating m-1"
               style={{ backgroundColor: "#ac2bac" }}
-              href="#!"
+              href="https://www.instagram.com/losrodriguez_tucuman/"
+              target="_blank"
+              rel="noopener noreferrer"
               role="button"
             >
               <i className="bi bi-instagram"></i>
             </a>
   
-            {/* Linkedin */}
-            <a
-              data-mdb-ripple-init
-              className="btn text-white btn-floating m-1"
-              style={{ backgroundColor: "#0082ca" }}
-              href="#!"
-              role="button"
-            >
-              <i className="bi bi-linkedin"></i>
-            </a>
-  
+ 
           </section>
           {/* Section: Social media */}
         </div>
