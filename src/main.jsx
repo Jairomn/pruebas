10e93ae1b6fde5abfb3239e2322201e2eb712c52
Jairo <jairomanueln@gmail.com>
2024-03-06 19:51:58 -0300

    modifi navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './stylesheets/index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
